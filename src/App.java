c9e364d1e662f0172b5e6d233b58fa97b9bde0c5
Santiago Passafiume <santiagopassafiume@protonmail.com>
2023-05-06 18:27:40 -0300

    add getUserInput function

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
@@ -18,6 +20,14 @@ public class App {
 
     public static float divide(float n1, float n2) {
         return n1 / n2;
+    }
+
+    public static int getUserInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(prompt);
+        int input = scanner.nextInt();
+        scanner.close();
+        return input;
     }
 
 }
