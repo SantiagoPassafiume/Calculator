a262494de0af5965ddf394b7dfee65bce2786a0d
Santiago Passafiume <santiagopassafiume@protonmail.com>
2023-05-06 18:24:34 -0300

    add sum, substract, multiply, and divide functions.

--- src/App.java
+++ src/App.java
@@ -4,4 +4,20 @@ public class App {
 
     }
 
+    public static float sum(float n1, float n2) {
+        return n1 + n2;
+    }
+
+    public static float substract(float n1, float n2) {
+        return n1 - n2;
+    }
+
+    public static float multiply(float n1, float n2) {
+        return n1 * n2;
+    }
+
+    public static float divide(float n1, float n2) {
+        return n1 / n2;
+    }
+
 }
