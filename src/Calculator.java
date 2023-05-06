862a6da37116ebd3320b00da913b2ad5cbbe671e
Santiago Passafiume <santiagopassafiume@protonmail.com>
2023-05-06 20:14:47 -0300

    Remove values from the declarations of n1 and n2

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
-        float n1 = 0;
-        float n2 = 0;
+        float n1;
+        float n2;
         char operation;
         Scanner scanner = new Scanner(System.in);
 
